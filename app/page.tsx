abc3f4b6618b2dd745e1ef007b1f9b11de97cd6a
Jack Martin <jackmartin.dev@gmail.com>
2024-08-17 12:12:06 +0900

    Add app height

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <div className="container border border-grey-300 rounded-l shadow-md p-0">
+    <div className="container h-3/4 border border-grey-300 rounded-l shadow-md p-0">
       <Sidebar />
     </div>
   );
